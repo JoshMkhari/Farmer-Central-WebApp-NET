bfb17470e39fc56b39a768f039ef9642ea753b25
Josh Mkhari <njmkhari@gmail.com>
2022-04-03 13:33:12 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ST1109348.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {    
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Farmer, Admin")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Only farmers here to add products " + User.Identity.Name;
+
+
+            return View();
+        }
+
+        [Authorize(Roles = "Employee, Admin")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Only employees here to add farmers and view products";
+
+            return View();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ST1109348.Startup))]
+namespace ST1109348
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
